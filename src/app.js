4426395943428786cd6daa197cf06c54acf7e8c9
frazhaidry <frazhaidry24@gmail.com>
2024-11-09 01:02:50 +0530

    API routing

--- src/app.js
+++ src/app.js
@@ -2,14 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello from the Dashboard")
-})
 
-app.get("/test", (req, res) => {
-    res.send("Hello from the test path");
-})
+app.post("/hello", (req, res) => {
+    res.send("Post call");
+});
+
+app.delete("/hello", (req, res) => {
+    res.send("Deleted successfully");
+});
+
+//Dynamic Routing (hello/123/fraz)
+app.get("/hello/:id/:name", (req, res) => {
+    console.log(req.params)
+    res.send({ firstName : "fraz", lastName: "Haidry"})
+});
+
+
+//To get the query send hello?userid=101&pass=123 in the postman get method
+app.get("/hello", (req, res) => {
+    console.log(req.query)
+    
+});
+
+
 
 app.listen(3000, ()=> {
     console.log("Serving is running on 3000")
-})
+});
