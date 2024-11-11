d123467209d9fedd10ec41a924af574c7d8b4efd
frazhaidry <frazhaidry24@gmail.com>
2024-11-11 18:47:37 +0530

    Password hash of new users

--- src/app.js
+++ src/app.js
@@ -2,21 +2,37 @@ const express = require("express");
 const connectDB = require("./config/database")
 const app = express();
 const User = require("./models/user")
+const { validateSignUpData } = require("./utils/validaton")
+const  bcrypt = require("bcrypt")
 
 app.use(express.json());
 
 //API - To add user into a database
 app.post("/signup", async (req, res) => {
-
-    console.log(req.body);
-    const user = new User(req.body);
-
     try { 
+    // Validation of data
+    validateSignUpData(req);
+    const {firstName, lastName, emailId, password} = req.body;
+
+    // Encrypt the password 
+    const passwordHash = await bcrypt.hash(password , 10);
+    console.log(passwordHash)
+
+    // Creating a new instance of the User Model
+       console.log(req.body);
+       const user = new User({
+        firstName,
+        lastName,
+        emailId,
+        password: passwordHash,
+       });
+
+    
         await user.save();
         res.send("User added successfully");
     } catch (error) {
-        console.error("Error adding user:", error);
-        res.status(500).send("An error occurred while adding the user.");
+        // console.error("Error adding user:", error);
+        res.status(500).send("ERROR : " + error.message);
     }
 });
 
