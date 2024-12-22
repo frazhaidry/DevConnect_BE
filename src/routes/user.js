0b0360350c28ec816e4e25e993e626139d22626b
frazhaidry <frazhaidry24@gmail.com>
2024-12-22 22:34:43 +0530

    API - user/connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -23,5 +23,38 @@ userRouter.get("/user/requests/received", userAuth, async(req, res) => {
     }
 })
 
+userRouter.get("/user/connections", userAuth, async(req, res) => {
+    try{
+        const loggedInUser = req.user;
 
+        console.log('Fetching connections...');
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [
+                 { toUserId: loggedInUser._id, status: "accepted" },
+                 { fromUserId: loggedInUser._id, status: "accepted" }
+             ]
+        })
+        .populate("fromUserId", ["firstName", "lastName"])
+        .populate("toUserId", ["firstName", "lastName"]);
+
+        console.log('Fetched connection requests:', connectionRequest);
+
+        console.log(connectionRequest);
+
+        const data = connectionRequest.map((row) => {
+
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        }
+    ); 
+ 
+        res.json({ data });
+
+    }
+    catch(error){
+        res.status(400).send({message: error.message});
+    }
+})
 module.exports = userRouter
